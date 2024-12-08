bdc5374760d79bfa58b93e4f3164a554b2fcd2ad
iamspa <iamspa@gmail.com>
2024-12-08 09:21:55 +0700

    Update websocket.js

--- utils/websocket.js
+++ utils/websocket.js
@@ -66,7 +66,7 @@ export async function createConnection(token) {
         setTimeout(() => {
             logger("Attempting to reconnect for token:", token, "warn");
             createConnection(token); 
-        }, 2500);
+        }, 5000);
     });
 
     socket.on("error", (error) => {
