79814ef1ceec0d314b5faed1b93dbcc936fdf9c7
iamspa <iamspa@gmail.com>
2024-12-10 19:02:01 +0700

    Update system.js

--- utils/system.js
+++ utils/system.js
@@ -53,10 +53,10 @@ function generateRandomCpuInfo() {
 
 function generateRandomGpuInfo() {
     const renderers = [
-        "ANGLE (Google, Vulkan 1.3.0 (SwiftShader Device (Subzero) (0x0000C0DE)), SwiftShader driver-5.0.0)",
+        "ANGLE (NVIDIA, NVIDIA GeForce GTX 1050 (0x00001C81) Direct3D11 vs_5_0 ps_5_0, D3D11-32.0.15.5612)",
 
     ];
-    const vendors = ["Google"];
+    const vendors = ["NVIDIA"];
     return {
         renderer: renderers[Math.floor(Math.random() * renderers.length)],
         vendor: vendors[Math.floor(Math.random() * vendors.length)]
